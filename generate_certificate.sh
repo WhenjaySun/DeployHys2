73ad8df1a87af1baac7b6def67ec30cc90395d87
hans <admin@huspace.cn>
2024-08-04 23:42:04 +0800

    INIT

---
+++ generate_certificate.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && sudo chown hysteria /etc/hysteria/server.key && sudo chown hysteria /etc/hysteria/server.crt
---
+++ test_proxy.sh
@@ -0,0 +1,2 @@
+curl --proxy http://user:pwd@hk6.huspace.cn:58080 https://www.baidu.com
+curl --socks4a localhost:1080 http://example.com
